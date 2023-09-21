20a06f33d48b5035a7d4f906f570289a30f0057c
Mehmet Yıldız <yildiz.mehmet@metu.edu.tr>
2023-09-21 17:57:17 +0300

    valueerror added

--- calculator.py
+++ calculator.py
@@ -1,16 +1,24 @@
 def add(x, y):
+    if x is None or y is None:
+        raise ValueError(f'Invalid parameters: x={x}, y={y}')
     return x + y
 
 
 def subtract(x, y):
+    if x is None or y is None:
+        raise ValueError(f'Invalid parameters: x={x}, y={y}')
     return x - y
 
 
 def multiply(x, y):
+    if x is None or y is None:
+        raise ValueError(f'Invalid parameters: x={x}, y={y}')
     return x * y
 
 
 def divide(x, y):
+    if x is None or y is None:
+        raise ValueError(f'Invalid parameters: x={x}, y={y}')
     if y == 0:
         raise ValueError('Can not divide by zero!')
     return x / y
